9377d17e68d04fd474281f4a2357ebb3554211fa
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-08-15 13:04:15 -0500

    cambio de variables de entorno a datos

--- models/connectionBD.js
+++ models/connectionBD.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b6953088f95f6e',
+    password: '34e14534',
+    database: 'heroku_9d2f46857d5c1c3'
+});
 
 connection.connect((err)=>{
     if(err){
