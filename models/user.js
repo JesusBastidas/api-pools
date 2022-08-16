5496333bcfedf19a2633c344ff08fa2b138197f0
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-08-15 21:11:02 -0500

    modificacion esquema users

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {type: String, required: true},
+    name: {type: String, required: false},
     email: {type: String, required: true},
     pass: {type: String, required: true},
-    rol: {type: String, required: true},
+    rol: {type: String, required: false},
 }, {versionKey: false});
 
 module.exports = mongoose.model('user', userSchema);
