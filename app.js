1c374c45b6f5c38ba133fdbf0f4bf4549c12daa8
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-08-16 16:42:00 -0500

    funcion de identificacion de usuario en app y user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const userRoutes = require('./routes/user');
+const user = require('./models/user');
 
 
 const app = express();
@@ -10,6 +11,8 @@ const CORS = require('cors');
 require('dotenv').config();
 
 app.use(CORS());
+app.use(bodyParser.json());
+app.use(bosyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use(express.json());
@@ -19,6 +22,40 @@ app.use('/api', require('./routes/supplies'));
 
 app.get('/',(req,res)=>{
     res.send('Hola caracola');
+});
+
+app.post('/register',(req,res) => {
+    const {name,email,pass}=req.body;
+    const user = new user({name,email,pass});
+
+    user.save(err=>{
+        if(err){
+            res.status(500).send('Error de registro');
+        }else{
+            res.status(200).send('Usuario registrado');
+        }
+    })
+});
+
+app.post('/auth', (req,res)=>{
+    const {email,pass}=req.body;
+    user.findOne({email},(err,user)=>{
+        if(err){
+            res.status(500).send('Error de autenticación');
+        }else if(!user){
+            res.status(500).send('Usuario no encontrado');
+        }else{
+            user.comparePass(pass,(err, result)=>{
+                if(err){
+                    res.status(500).send('Error de autenticación');
+                }else if(result){
+                    res.status(200).send('Usuario autenticado');
+                }else{
+                    res.status(500).send('Mail o constraseña incorrectos');
+                }
+            })
+        }
+    })
 })
 
 mongoose.connect(process.env.DB_CONNECTION).then(()=>console.log('conectado a mongo')).catch(err=>console.log(err)); 
--- models/user.js
+++ models/user.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); //para encriptar la contraseña
+
+const saltRound = 10; //numero de veces que se va a encriptar la contraseña
 
 const userSchema = mongoose.Schema({
     name: {type: String, required: false},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     pass: {type: String, required: true},
     rol: {type: String, required: false},
 }, {versionKey: false});
+
+userSchema.pre('save', function(next){
+    if (this.isNew || this.isModified('pass')){
+        const document = this;
+        bcrypt.hash(document.pass, saltRound, function(err, hashedPassword){
+            if (err)
+                next(err);
+            else
+                document.pass = hashedPassword;
+                next();
+        }
+        );
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.comparePass = (pass, callback)=>{
+    bcrypt.compare(pass, this.pass, function(err, same){
+        if (err){
+            callback(err);
+        }else{
+            callback(err, same);
+        }
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
     
