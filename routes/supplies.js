175bc548a22bac15ac6c19241a86f721bc282aa0
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-08-17 18:53:28 -0500

    find by id en supplies

--- routes/supplies.js
+++ routes/supplies.js
@@ -6,6 +6,10 @@ suppliesRouter.get('/supplies', (req, res) => {
     suppliesSchema.find().then((data) => res.json(data)).catch((err) => res.json(err));
 });
 
+suppliesRouter.get('/supplies/:id', (req, res) => {
+    suppliesSchema.findById(req.params.id).then((data) => res.json(data)).catch((err) => res.json(err));
+});
+
 suppliesRouter.post('/supplies', (req, res) => {
     suppliesSchema(req.body).save().then((data) => res.json(data)).catch((err) => res.json(err));
 });
