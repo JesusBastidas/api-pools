d3a4de9faa7f44a84e79924ae25d819fc4c04f17
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-08-16 22:00:01 -0500

    incluir code a productos e insumos

--- models/products.js
+++ models/products.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
+    code: {type: String, required: true},
     name: {type: String, required: true},
     description: {type: String, required: false},
     quantity: {type: Number, required: true},
--- models/supplies.js
+++ models/supplies.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const supliesSchema = mongoose.Schema({
+    code: {type: String, required: true},
     name: {type: String, required: true},
     description: {type: String, required: false},
     quantity: {type: Number, required: true},
